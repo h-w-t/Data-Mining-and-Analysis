aa62928759a75e6f0e41b1fbaf29d5b7ead5ec08
The ordinary one <alex13661616831@gmail.com>
2023-06-21 14:47:29 +0800

    初始化数据库

--- backend/apis/models.py
+++ backend/apis/models.py
@@ -7,8 +7,11 @@ class OrderSum(models.Model):
     orderNum = models.IntegerField()
     city = models.CharField(max_length=50)
 
+    def __str__(self):
+        return self.orderNum
+
     class Meta:
-        db_table = 'orderSum'
+        db_table = 'ordersum'
 
 
 class PositionInfo(models.Model):
@@ -16,5 +19,23 @@ class PositionInfo(models.Model):
     post = models.CharField(max_length=20, null=True)
     level = models.CharField(max_length=20, null=True)
 
+    def __str__(self):
+        return self.post
+    
     class Meta:
-        db_table = 'positionInfo'
+        db_table = 'positioninfo'
+
+
+class Iris(models.Model):
+    id = models.IntegerField(primary_key=True)
+    SpeL = models.FloatField()
+    SpeW = models.FloatField()
+    PetL = models.FloatField()
+    PetW = models.FloatField()
+    Species = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.Species
+
+    class Meta:
+        db_table = 'iris'
