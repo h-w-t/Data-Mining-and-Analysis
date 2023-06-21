7e06221e06e22ee8641d1b4a7c001d5a49cb09cc
The ordinary one <alex13661616831@gmail.com>
2023-06-21 12:07:07 +0800

    项目重构

--- backend/apis/models.py
+++ backend/apis/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
+
 # Create your models here.
+class OrderSum(models.Model):
+    id = models.IntegerField(primary_key=True)
+    orderNum = models.IntegerField()
+    city = models.CharField(max_length=50)
+
+    class Meta:
+        db_table = 'orderSum'
+
+
+class PositionInfo(models.Model):
+    id = models.IntegerField(primary_key=True)
+    post = models.CharField(max_length=20, null=True)
+    level = models.CharField(max_length=20, null=True)
+
+    class Meta:
+        db_table = 'positionInfo'
